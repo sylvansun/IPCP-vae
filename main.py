7feca2b888a8c4ed1740de4c69c85f786a8cf712
SylvanSun <yilin.sun@sjtu.edu.cn>
2023-05-07 16:26:17 +0800

    merge codes together

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import os
 import torch
 from torch import nn
 import torch.nn.functional as F
 import torchvision
 from torch.utils.data import DataLoader
-import utils
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 class VAE(nn.Module):
-    """Implementation of VAE(Variational Auto-Encoder)"""
-    def __init__(self):
+    def __init__(self,in_dim=784, h_dim=256, z_dim=2):
         super(VAE, self).__init__()
 
         self.fc1 = nn.Linear(784, 256)
@@ -25,12 +25,12 @@ class VAE(nn.Module):
 
     def decode(self, z):
         h3 = F.relu(self.fc3(z))
-        recon = torch.sigmoid(self.fc4(h3))  # use sigmoid because the input image's pixel is between 0-1
+        recon = torch.sigmoid(self.fc4(h3))
         return recon
 
     def reparametrize(self, mu, log_std):
         std = torch.exp(log_std)
-        eps = torch.randn_like(std)  # sample from standard normal distribution
+        eps = torch.randn_like(std)
         z = mu + eps * std
         return z
 
@@ -40,13 +40,21 @@ class VAE(nn.Module):
         recon = self.decode(z)
         return recon, mu, log_std
 
-    def loss_function(self, recon, x, mu, log_std) -> torch.Tensor:
-        recon_loss = F.mse_loss(recon, x, reduction="sum")  # use "mean" may have a bad effect on gradients
+    def loss_function(self, recon, x, mu, log_std):
+        recon_loss = F.mse_loss(recon, x, reduction="sum") 
         kl_loss = -0.5 * (1 + 2*log_std - mu.pow(2) - torch.exp(2*log_std))
         kl_loss = torch.sum(kl_loss)
         loss = recon_loss + kl_loss
         return loss
 
+def to_img(x):
+    x = x.clamp(0, 1)
+    imgs = x.reshape(x.shape[0], 1, 28, 28)
+    return imgs
+
+def make_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 if __name__ == '__main__':
     epochs = 100
@@ -55,8 +63,9 @@ if __name__ == '__main__':
     recon = None
     img = None
 
-    utils.make_dir("./img/vae")
-    utils.make_dir("./model_weights/vae")
+    
+    make_dir("./img/vae")
+    make_dir("./model_weights/vae")
 
     train_data = torchvision.datasets.MNIST(
         root='./data',
@@ -97,20 +106,14 @@ if __name__ == '__main__':
 
         # save imgs
         if epoch % 10 == 0:
-            imgs = utils.to_img(recon.detach())
+            imgs = to_img(recon.detach())
             print(recon.shape)
             path = "./img/vae/epoch{}.png".format(epoch+1)
             torchvision.utils.save_image(imgs, path, nrow=10)
             print("save:", path, "\n")
-            gaussian = torch.linspace(-5, 5, 800).view(-1, 2).to(device)
             g = (torch.tensor([(x, y) for y in range(20) for x in range(20)]).to(device,dtype=torch.float32).reshape((400, 2)) - 10)/2
-            # print(gaussian)
-            # print(gaussian.shape)
-            # print(g.shape)
-            # print(g)
-            # assert False
             linear_recons = vae.decode(g)
-            linear_imgs = utils.to_img(linear_recons.detach())
+            linear_imgs = to_img(linear_recons.detach())
             linear_path = "./img/vae/linear_epoch{}.png".format(epoch+1)
             torchvision.utils.save_image(linear_imgs, linear_path, nrow=20)
             print("save:", linear_path, "\n")
@@ -119,4 +122,4 @@ if __name__ == '__main__':
     print("save raw image:./img/vae/raw/png", "\n")
 
     # save val model
-    utils.save_model(vae, "./model_weights/vae/vae_weights.pth")
+    torch.save(vae, "./model_weights/vae/vae_weights.pth")
